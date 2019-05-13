0a86c193d493cea903cf50ab6e2263c11fae210d
Phillip DiPhillipo <diphillipophillip@gmail.com>
2019-05-13 15:42:34 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
 // Code your solution in this file!
 const companyName = 'Scuber';
-let mostProfitableNeighborhood = 'Chelsea';
+let mostProfitableNeighborhood = 'Chelsea'; 
+const companyCeo = "Susan Smith";
